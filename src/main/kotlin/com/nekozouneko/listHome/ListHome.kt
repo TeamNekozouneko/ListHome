26d96a658a3bef1d84cc1e6ce1ab4cdbbc53e53b
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-05-24 15:11:04 +0900

    init

---
+++ src/main/kotlin/com/nekozouneko/listHome/ListHome.kt
@@ -0,0 +1,14 @@
+package com.nekozouneko.listHome
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ListHome : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
